96da19063ffa6f2c2cf9e9c204617dc27ae29a95
solenova0 <adugnasolan@gmail.com>
2026-02-04 01:49:25 +0300

    Add Hackerrank solution files

---
+++ Hackerrank/Mo_Power.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
+    a = int(input().strip())
+    b = int(input().strip())
+    m = int(input().strip())
+
+    print(pow(a, b))
+    print(pow(a, b, m))
