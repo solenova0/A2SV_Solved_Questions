79262dc3a2c83d32a75b807bc1da0950b36f0d70
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-02-05 14:56:23 +0300

    Time: 7 ms (47.22%) | Memory: 19.4 MB (20.83%) - LeetSync

---
+++ Leetcode/299-bulls-and-cows/bulls-and-cows.py
@@ -0,0 +1,24 @@
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        hm = {}
+        for v in secret:
+            if v in hm:
+                hm[v] +=1
+            else:
+                hm[v] = 1
+        bull = 0
+        cow = 0
+        print(hm)
+        for i in range(len(guess)):
+            if secret[i] == guess[i]:
+                bull += 1
+        for k in guess:        
+            if k in hm and hm[k] != 0:
+                cow += 1
+                hm[k] -= 1
+        return str(bull)+'A'+str(cow-bull)+'B'
+
+            
+
+        
+        
