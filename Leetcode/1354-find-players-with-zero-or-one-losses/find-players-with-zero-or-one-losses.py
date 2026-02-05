140b066f3d6b4caf96f48baad07dcfa4b5fa02c7
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-02-05 16:17:56 +0300

    Time: 122 ms (64.37%) | Memory: 64.4 MB (17.82%) - LeetSync

---
+++ Leetcode/1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findWinners(self, matches):
+        freq = {}
+        ans = []
+        res = []
+        players = set()
+
+        # count losses
+        for i in range(len(matches)):
+            winner = matches[i][0]
+            loser = matches[i][1]
+            players.add(winner)
+            players.add(loser)
+            freq[loser] = freq.get(loser, 0) + 1
+
+        for p in players:
+            if freq.get(p, 0) == 0:
+                ans.append(p)
+            elif freq[p] == 1:
+                res.append(p)
+
+        return [sorted(ans), sorted(res)]
