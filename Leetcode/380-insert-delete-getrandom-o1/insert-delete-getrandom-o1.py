2ac121ed21f846f76d86463da62a00e74f3cf129
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-02-10 10:23:05 +0300

    Time: 96 ms (83.70%) | Memory: 57.2 MB (25.22%) - LeetSync

---
+++ Leetcode/380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -0,0 +1,29 @@
+import random
+class RandomizedSet:
+    def __init__(self):
+        self.index = {}  
+        self.nums = []          
+
+    def insert(self, val: int) -> bool:
+        if val in self.index:
+            return False
+        self.nums.append(val)
+        self.index[val] = len(self.nums) - 1
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.index:
+            return False
+        
+        idx = self.index[val]
+        last_val = self.nums[-1]
+
+        self.nums[idx] = last_val
+        self.index[last_val] = idx
+
+        self.nums.pop()
+        del self.index[val]
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.nums)
