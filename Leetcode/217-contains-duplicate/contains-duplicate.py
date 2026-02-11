601685ff4f6ca4637e73e2d3960de6cfb263d486
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-02-11 09:13:06 +0300

    Time: 19 ms (26.38%) | Memory: 36.3 MB (7.00%) - LeetSync

---
+++ Leetcode/217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        freq = {}
+        for v in nums:
+            freq[v] = freq.get(v,0) + 1
+            if freq[v] > 1:
+                return True
+        return False
+
+        
