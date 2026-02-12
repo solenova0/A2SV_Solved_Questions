b94d37e23c94fb66f307228b2a748b8befecd153
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-02-12 09:25:03 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (9.10%) - LeetSync

--- Leetcode/349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ Leetcode/349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -7,19 +7,3 @@ class Solution:
             if v in nums2Set:
                 res.append(v)
         return res
-        # nums1.sort()
-        # nums2.sort()
-        # i = j = 0
-        # result = set()
-        
-        # while i < len(nums1) and j < len(nums2):
-        #     if nums1[i] == nums2[j]:
-        #         result.add(nums1[i])
-        #         i += 1
-        #         j += 1
-        #     elif nums1[i] < nums2[j]:
-        #         i += 1
-        #     else:
-        #         j += 1
-        # return list(result)
-        #     w
