9b5a554e63f72ef1c8e3902d925d8e408d864f06
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-02-18 16:12:01 +0300

    Time: 3 ms (57.25%) | Memory: 19.4 MB (21.51%) - LeetSync

---
+++ Leetcode/179-largest-number/largest-number.py
@@ -0,0 +1,26 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i, n in enumerate(nums):
+            nums[i] = str(n)
+
+        def compare(n1, n2):
+            if n1 + n2 > n2 + n1:
+                return -1
+            else:
+                return 1
+
+        nums = sorted(nums, key = cmp_to_key(compare))
+
+        return str(int("".join(nums)))
+        # nums = [str(n) for n in nums]
+        
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(0, n - i - 1):
+        #         if nums[j] + nums[j+1] < nums[j+1] + nums[j]:
+        #             nums[j], nums[j+1] = nums[j+1], nums[j]
+
+        # if nums[0] == "0":
+        #     return "0"
+
+        # return "".join(nums)
