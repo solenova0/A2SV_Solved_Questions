d73dc380f831e9746eefd76f61a1f675e9c6cf1f
solenova0 <adugnasolan@gmail.com>
2026-02-19 21:38:06 +0300

    Refactor comments in spiralOrder method for clarity

--- Leetcode/54-spiral-matrix/spiral-matrix.py
+++ Leetcode/54-spiral-matrix/spiral-matrix.py
@@ -8,23 +8,22 @@ class Solution:
         left, right = 0, len(matrix[0])
         top, bottom = 0, len(matrix)
 
-        while left < right and top < bottom: # get top row
+        while left < right and top < bottom:
             for i in range(left, right):
                 res.append(matrix[top][i])
             top += 1
            
-            for i in range(top, bottom): # get right col
+            for i in range(top, bottom):
                 res.append(matrix[i][right - 1])
             right -= 1
             if not (left < right and top < bottom):
-                #cases single row or col left
                 break
           
-            for i in range(right - 1, left - 1, -1):  # get bottom row
+            for i in range(right - 1, left - 1, -1): 
                 res.append(matrix[bottom - 1][i])
             bottom -= 1
             
-            for i in range(bottom - 1, top - 1, -1):# get left col
+            for i in range(bottom - 1, top - 1, -1):
                 res.append(matrix[i][left])
             left += 1
 
