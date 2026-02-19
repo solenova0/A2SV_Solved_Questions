4717f265906ddf382f7f851bbba5f787feeaf2b3
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-02-19 21:36:36 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (73.85%) - LeetSync

--- Leetcode/1009-pancake-sorting/pancake-sorting.py
+++ Leetcode/1009-pancake-sorting/pancake-sorting.py
@@ -1,31 +1,16 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
-        n = len(arr)
-        res = []
-        for i in range(n-1,0,-1):
-            maxx = max(arr[:i+1])
-            if maxx == arr[i]:
+        ans = []
+        for n in range(len(arr), 1, -1):
+            i = arr.index(n)
+            if n == i + 1: 
                 continue
-            elif maxx == arr[0]:
-                res.append(i+1)
-                temp = (arr[:i+1])
-                temp.reverse()
-                for j in range(i+1):
-                    arr[j] = temp[j]
-                continue
-            for j in range(i):
-                if arr[j] == maxx:
-                    res.append(j+1)
-                    res.append(i+1)
-                    temp = arr[j+1:i+1]
-                    a,b = i,j
-                    while b >= 0:
-                        arr[a] = arr[b]
-                        b -= 1
-                        a -= 1
-                    for k in range(len(temp)):
-                        arr[a] = temp[k]
-                        a -= 1
-                    break         
-        return res
+
+            if i != 0:
+                ans.append(i+1)
+            ans.append(n) 
+
+            arr[:i+1] = reversed(arr[:i+1])
+            arr[:n] = reversed(arr[:n])
         
+        return ans
