e654f8aba45b88c8cfac478174f91ffc5f9eb776
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-02-20 16:10:40 +0300

    Time: 3 ms (25.59%) | Memory: 19.3 MB (75.80%) - LeetSync

---
+++ Leetcode/274-h-index/h-index.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        arr = [0 for _ in range(n + 1)]
+
+        for i,v in enumerate(citations):
+            if v > n :
+                arr[n] += 1
+            else:
+                arr[v] += 1
+        
+        total = 0
+        for i in range(n, -1, -1):
+            total += arr[i]
+            if total >= i:
+                return i
