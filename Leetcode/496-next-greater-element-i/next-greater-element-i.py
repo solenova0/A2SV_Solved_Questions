7dfdb6f7104cba7f0071e1f4408f86dc30743d78
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-03-10 19:48:49 +0300

    Time: 0 ms (100.00%) | Memory: 19.6 MB (28.02%) - LeetSync

--- Leetcode/496-next-greater-element-i/next-greater-element-i.py
+++ Leetcode/496-next-greater-element-i/next-greater-element-i.py
@@ -1,14 +1,15 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        freq = {}
+        freq = defaultdict(lambda:-1)
         stack = []
-        for num in nums2:
-            while stack and stack[-1] < num:
-                prev = stack.pop()
-                freq[prev] = num
-            stack.append(num)
+        
+        for v in nums2:
+            while stack and v > stack[-1]:
+                val = stack.pop()
+                freq[val] = v
+            stack.append(v)
 
-        for num in stack:
-            freq[num] = -1
-
-        return [freq[num] for num in nums1]
+        ans = []
+        for i in nums1:
+            ans.append(freq[i])
+        return ans
