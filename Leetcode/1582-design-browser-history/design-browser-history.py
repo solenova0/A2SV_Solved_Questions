eb7526a88ab95087f77d647a1eac0322811d13c6
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-03-11 17:48:03 +0300

    Time: 43 ms (45.29%) | Memory: 22.2 MB (34.37%) - LeetSync

--- Leetcode/1582-design-browser-history/design-browser-history.py
+++ Leetcode/1582-design-browser-history/design-browser-history.py
@@ -4,49 +4,49 @@ class ListNode:
         self.prev = prev
         self.next = next
 
-# class BrowserHistory:
-#     def __init__(self, homepage: str):
-#         self.cur = ListNode(homepage)
-
-#     def visit(self, url: str) -> None:
-#         self.cur.next = ListNode(url, self.cur)
-#         self.cur = self.cur.next
-
-#     def back(self, steps: int) -> str:
-#         while self.cur.prev and steps > 0:
-#             self.cur = self.cur.prev
-#             steps -= 1
-#         return self.cur.val
-
-#     def forward(self, steps: int) -> str:
-#         while self.cur.next and steps > 0:
-#             self.cur = self.cur.next
-#             steps -= 1
-#         return self.cur.val
-
-
-# Array Implementation
-class BrowserHistory:    
+class BrowserHistory:
     def __init__(self, homepage: str):
-        self.i = 0
-        self.len = 1
-        self.his = [homepage]
+        self.cur = ListNode(homepage)
 
     def visit(self, url: str) -> None:
-        if len(self.his) < self.i + 2:
-            self.his.append(url)
-        else:
-            self.his[self.i + 1] = url
-        self.i += 1
-        self.len = self.i + 1
+        self.cur.next = ListNode(url, self.cur)
+        self.cur = self.cur.next
 
     def back(self, steps: int) -> str:
-        self.i = max(self.i - steps, 0)
-        return self.his[self.i]
+        while self.cur.prev and steps > 0:
+            self.cur = self.cur.prev
+            steps -= 1
+        return self.cur.val
 
     def forward(self, steps: int) -> str:
-        self.i = min(self.i + steps, self.len - 1)
-        return self.his[self.i]
+        while self.cur.next and steps > 0:
+            self.cur = self.cur.next
+            steps -= 1
+        return self.cur.val
+
+
+# # Array Implementation
+# class BrowserHistory:    
+#     def __init__(self, homepage: str):
+#         self.i = 0
+#         self.len = 1
+#         self.his = [homepage]
+
+#     def visit(self, url: str) -> None:
+#         if len(self.his) < self.i + 2:
+#             self.his.append(url)
+#         else:
+#             self.his[self.i + 1] = url
+#         self.i += 1
+#         self.len = self.i + 1
+
+#     def back(self, steps: int) -> str:
+#         self.i = max(self.i - steps, 0)
+#         return self.his[self.i]
+
+#     def forward(self, steps: int) -> str:
+#         self.i = min(self.i + steps, self.len - 1)
+#         return self.his[self.i]
 
 
 
