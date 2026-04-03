68156d5401bd47931fe336262e276a4af5bc0f61
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-04-03 17:07:50 +0300

    Time: 54 ms (6.20%) | Memory: 19.2 MB (79.64%) - LeetSync

--- Leetcode/278-first-bad-version/first-bad-version.py
+++ Leetcode/278-first-bad-version/first-bad-version.py
@@ -2,12 +2,15 @@
 # def isBadVersion(version: int) -> bool:
 
 class Solution:
-    def firstBadVersion(self, n: int) -> int:
-        l, r = 1, n
-        while l < r:
-            v = (l + r) // 2
-            if isBadVersion(v):
-                r = v
-            else:
-                l = v + 1
-        return l
+  def firstBadVersion(self, n: int) -> int:
+    l = 1
+    r = n
+    while l < r:
+      m = (l + r) >> 1
+      if isBadVersion(m):
+        r = m
+      else:
+        l = m + 1
+
+    return l
+        
