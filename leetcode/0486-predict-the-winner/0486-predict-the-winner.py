02be70af0a088806e1dff30b1d2f5ad047caa800
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-04-12 13:16:21 +0300

    Add solution for Predict the Winner

--- leetcode/0486-predict-the-winner/0486-predict-the-winner.py
+++ leetcode/0486-predict-the-winner/0486-predict-the-winner.py
@@ -3,10 +3,8 @@ class Solution:
         def helper(i, j):
             if i > j:
                 return 0
-            
             if i == j:
                 return nums[i]
-            
             return max(
                 nums[i] - helper(i + 1, j),
                 nums[j] - helper(i, j - 1)
