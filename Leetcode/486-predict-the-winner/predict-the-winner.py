c0d93f0feb7c1f2cc316f6c46f76c5d3fbc2ab98
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-04-12 13:13:31 +0300

    Time: 1704 ms (11.91%) | Memory: 19.3 MB (84.23%) - LeetSync

---
+++ Leetcode/486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,15 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        def helper(i, j):
+            if i > j:
+                return 0
+            
+            if i == j:
+                return nums[i]
+            
+            return max(
+                nums[i] - helper(i + 1, j),
+                nums[j] - helper(i, j - 1)
+            )
+        
+        return helper(0, len(nums) - 1) >= 0
