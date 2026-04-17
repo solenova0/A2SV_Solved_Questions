a92c948f3eb3fd6ff4705e4178792d6afbb46219
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-04-17 22:45:56 +0300

    Add solution for Create Sorted Array through Instructions

--- leetcode/1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
+++ leetcode/1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
@@ -1,13 +1,30 @@
+#Fenwick Tree Algorthim
+class BIT:
+    def __init__(self, n):
+        self.n = n
+        self.tree = [0]*(n+1)
+    def update(self, i):
+        while i <= self.n:
+            self.tree[i] += 1
+            i += i & -i
+    def query(self, i):
+        s = 0
+        while i > 0:
+            s += self.tree[i]
+            i -= i & -i
+        return s
+
+
 class Solution:
-    def createSortedArray(self, instructions: List[int]) -> int:
-        arr = []
-        cost = 0
+    def createSortedArray(self, instructions):
+        MOD = 10**9 + 7
+        bit = BIT(max(instructions))
 
-        for x in instructions:
-            left = bisect.bisect_left(arr, x)
-            right = len(arr) - bisect.bisect_right(arr, x)
+        res = 0
+        for i, x in enumerate(instructions):
+            left = bit.query(x - 1)
+            right = i - bit.query(x)
+            res += min(left, right)
+            bit.update(x)
 
-            cost += min(left, right)
-            bisect.insort(arr, x)
-
-        return cost % (10**9 + 7)
+        return res % MOD
