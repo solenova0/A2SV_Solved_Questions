7e687c3d5dcfdaea1cff8ea39df29f3cf7bbccf3
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-04-27 14:27:08 +0300

    Time: 14 ms (90.92%) | Memory: 25.2 MB (63.47%) - LeetSync

---
+++ Leetcode/491-non-decreasing-subsequences/non-decreasing-subsequences.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def dfs(start, path):
+            if len(path) >= 2:
+                res.append(path[:])
+            used = set()  
+
+            for i in range(start, len(nums)):
+                if nums[i] in used:
+                    continue
+
+                if not path or nums[i] >= path[-1]:
+                    used.add(nums[i])
+                    path.append(nums[i])
+                    dfs(i + 1, path)
+                    path.pop()
+
+        dfs(0, [])
+        return res
