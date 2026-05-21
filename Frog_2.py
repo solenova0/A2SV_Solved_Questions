3611b82fedfb27e54aa1bf8e3c80d4a898da2f2e
Solan <adugnasolan@gmail.com>
2026-05-21 15:17:37 +0300

    Add solutions for AtCoder Frog 1 and Frog 2 problems

---
+++ Frog_2.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+h = list(map(int, input().split()))
+dp = [float('inf')] * n
+
+dp[0] = 0
+for i in range(1, n):
+    for j in range(1, k + 1):
+        if i - j >= 0:
+            dp[i] = min(dp[i],dp[i - j] + abs(h[i] - h[i - j])
+            )
+
+print(dp[-1])
