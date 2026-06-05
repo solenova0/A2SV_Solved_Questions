593a922566bb4934a080060b99585048079966d6
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-06-05 09:08:30 +0300

    Time: 1 ms (62.96%) | Memory: 19.1 MB (98.85%) - LeetSync

--- Leetcode/389-find-the-difference/find-the-difference.py
+++ Leetcode/389-find-the-difference/find-the-difference.py
@@ -1,5 +1,15 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
+        # for c in t:
+        #     if t.count(c) > s.count(c):
+        #         return(c)
+        freq = {}
         for c in t:
-            if t.count(c) > s.count(c):
-                return(c)
+            freq[c] = freq.get(c, 0) + 1
+
+        for c in s:
+            freq[c] -= 1
+            if freq[c] == 0:
+                del freq[c]
+
+        return list(freq.keys())[0]
