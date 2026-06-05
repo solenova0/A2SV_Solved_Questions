3db00fc1293f6d33c3452210b06979331383e811
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2026-06-05 09:27:25 +0300

    Time: 7 ms (28.71%) | Memory: 26.1 MB (6.65%) - LeetSync

--- Leetcode/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ Leetcode/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,16 +1,39 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        hold = -prices[0]
-        sold = 0
-        rest = 0
+        # hold = -prices[0]
+        # sold = 0
+        # rest = 0
 
-        for price in prices[1:]:
-            prev_hold = hold
-            prev_sold = sold
-            prev_rest = rest
+        # for price in prices[1:]:
+        #     prev_hold = hold
+        #     prev_sold = sold
+        #     prev_rest = rest
 
-            hold = max(prev_hold, prev_rest - price)
-            sold = prev_hold + price
-            rest = max(prev_rest, prev_sold)
+        #     hold = max(prev_hold, prev_rest - price)
+        #     sold = prev_hold + price
+        #     rest = max(prev_rest, prev_sold)
 
-        return max(sold, rest)
+        # return max(sold, rest)
+        n = len(prices)
+
+        @cache
+        def dfs(i, holding):
+            if i >= n:
+                return 0
+
+            ans = dfs(i + 1, holding)
+
+            if holding:
+                ans = max(
+                    ans,
+                    prices[i] + dfs(i + 2, 0) 
+                )
+            else:
+                ans = max(
+                    ans,
+                    -prices[i] + dfs(i + 1, 1)
+                )
+
+            return ans
+
+        return dfs(0, 0)
