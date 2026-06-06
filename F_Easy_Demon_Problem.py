d35832a5cbb77370236cddde92714c538297c6ff
Solan <adugnasolan@gmail.com>
2026-06-06 19:45:15 +0300

    ...

--- F_Easy_Demon_Problem.py
+++ F_Easy_Demon_Problem.py
@@ -1,4 +1,6 @@
 
+from math import isqrt
+
 n, m, q = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
@@ -14,13 +16,32 @@ for _ in range(q):
 
     flag = False
 
-    for i in range(1, int(abs(x)**0.5) + 1):
-        if x % i == 0:
-            d1 = i
-            d2 = x // i
+    if x == 0:
+        flag = 0 in sa or 0 in sb
+    else:
+        limit = isqrt(abs(x))
+        for i in range(1, limit + 1):
+            if abs(x) % i == 0:
+                d1 = i
+                d2 = abs(x) // i
 
-            if (d1 in sa and d2 in sb) or (d2 in sa and d1 in sb):
-                flag = True
-                break
+                if x > 0:
+                    if (
+                        (d1 in sa and d2 in sb)
+                        or (d2 in sa and d1 in sb)
+                        or (-d1 in sa and -d2 in sb)
+                        or (-d2 in sa and -d1 in sb)
+                    ):
+                        flag = True
+                        break
+                else:
+                    if (
+                        (d1 in sa and -d2 in sb)
+                        or (-d2 in sa and d1 in sb)
+                        or (d2 in sa and -d1 in sb)
+                        or (-d1 in sa and d2 in sb)
+                    ):
+                        flag = True
+                        break
 
     print("YES" if flag else "NO")
